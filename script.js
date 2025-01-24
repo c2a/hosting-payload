a5ed1e1d4c5e4bb1134237e1fb8538ee5870f29e
accalon <120159882+c2a@users.noreply.github.com>
2025-01-24 09:56:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is your external script file, /script.js
+console.log("Document domain is: " + document.domain);
+// This is your external script file, /script.js
+console.log("Document domain is: " + document.cookie);
